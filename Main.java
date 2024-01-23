c98bc7781d359f4e5c3bcfce9112965e125f3285
Adprtm188 <156062888+Adprtm188@users.noreply.github.com>
2024-01-23 11:53:36 +0700

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,41 +1,55 @@
-package com.mycompany.bangun_datar;
+package com.mycompany.bangun_ruang;
+import java.util.Scanner;
 public class Main {
-      public static void main(String[] args) {
-        
- 
-        BangunDatar bangunDatar = new BangunDatar();
-        
 
-        Persegi persegi = new Persegi();
-        persegi.sisi = 2;
+    public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    System.out.print("Silahkan Memasukkan Bangun Ruang (Bola, Kubus, Tabung, Limas Segitiga): ");
+    String br = input.nextLine();
         
-
-        Lingkaran lingkaran = new Lingkaran();
-        lingkaran.r = 22;
+    Bangun_Ruang bangunruang = new Bangun_Ruang();
+    switch(br){
         
-
-        PersegiPanjang persegiPanjang = new PersegiPanjang();
-        persegiPanjang.panjang = 8;
-        persegiPanjang.lebar = 4;
-        
-
-        Segitiga mSegitiga = new Segitiga();
-        mSegitiga.alas = 12;
-        mSegitiga.tinggi = 8;
-
-        bangunDatar.luas();
-        bangunDatar.keliling();
-        
-        persegi.luas();
-        persegi.keliling();
-        
-        lingkaran.luas();
-        lingkaran.keliling();
-        
-        persegiPanjang.luas();
-        persegiPanjang.keliling();
-        
-        mSegitiga.luas();
-        mSegitiga.keliling();
+    case "Bola":
+    Bola bola = new Bola();
+    System.out.print("Masukan nilai jari-jari (r) : ");
+    double er = input.nextDouble();
+    bola.r = er;
+    bola.volume();
+    break;
+    
+    case "Kubus":
+    Kubus kubus = new Kubus();
+    System.out.print("Masukan nilai sisi (s) : ");
+    double es = input.nextDouble();
+    kubus.S = es;
+    kubus.volume();
+    break;
+    
+    case "Tabung":
+    Tabung tabung = new Tabung();
+        System.out.print("Masukan nilai tinggi (t) : ");
+        double te = input.nextDouble();
+        System.out.print("Masukan nilai jari-jari (r) : ");
+        double je = input.nextDouble();
+    tabung.r = te;
+    tabung.t = je;
+    tabung.volume();
+    break;
+    
+    case "Limas Segitiga":
+    LimasSegitiga limassegitiga = new LimasSegitiga();
+        System.out.print("Masukan nilai luas alas (LA) : ");
+        double el = input.nextDouble();
+        System.out.print("Masukan nilai tinggi limas (TL) : ");
+        double ti = input.nextDouble();
+    limassegitiga.LA = el;
+    limassegitiga.TL = ti;
+    limassegitiga.volume();
+    break;
+    
+    default:
+    System.out.println("Rumus Tidak Ada!");
     }
+  }
 }
